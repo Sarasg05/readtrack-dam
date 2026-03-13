ed32bfc2ebf09b3cf85ae54fdc1b5ab4fca57149
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-03-13 10:56:38 +0100

    Registrar los modelos en admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AnnualGoal, Author, Genre, Book, Reading, ReadingSession
 
-# Register your models here.
+admin.site.register(AnnualGoal)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(Reading)
+admin.site.register(ReadingSession)
