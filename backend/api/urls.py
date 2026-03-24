4feb3f00a732488a90e8248dac61a6a68bff46aa
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-03-24 13:44:38 +0100

    annual_goals URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,10 +5,11 @@ from .views import (
     GenreViewSet,
     books,
     book_by_id,
+    annual_goals,
+    annual_goal_by_id,
     readings,
     reading_by_id,
     ReadingSessionViewSet,
-    AnnualGoalViewSet
 )
 
 
@@ -16,6 +17,9 @@ from .views import (
 urlpatterns = [
     path('books/', books),
     path('books/<int:id>/', book_by_id),
+
+    path('annual_goals/', annual_goals),
+    path('annual_goals/<int:id>/', annual_goal_by_id),
 
     path('readings/', readings),
     path('readings/<int:id>/', reading_by_id),
