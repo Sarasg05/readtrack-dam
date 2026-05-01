c8459d793ae35e9569d6fa2601e4e284576bab22
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-01 12:41:03 +0200

    Create Reading model

---
+++ android/app/src/main/java/com/ssg/readtrack/model/Reading.java
@@ -0,0 +1,9 @@
+package com.ssg.readtrack.model;
+
+public class Reading {
+    public int id;
+    public String book;
+    public String status;
+    public String start_date;
+    public String end_date;
+}
