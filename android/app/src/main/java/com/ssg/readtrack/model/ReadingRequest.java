4f1ee8289c590cdcf9af16419d9428645273b523
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-06 15:08:58 +0200

    Change ReadingRequest

--- android/app/src/main/java/com/ssg/readtrack/model/ReadingRequest.java
+++ android/app/src/main/java/com/ssg/readtrack/model/ReadingRequest.java
@@ -1,12 +1,10 @@
 package com.ssg.readtrack.model;
 
 public class ReadingRequest {
-    public int user;
     public int book;
     public String status;
 
     public ReadingRequest(int user, int book, String status) {
-        this.user = user;
         this.book = book;
         this.status = status;
     }
