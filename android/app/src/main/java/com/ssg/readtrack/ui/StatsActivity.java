2e326681e52784cdfe7181f2feb601b0f7808495
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-06 16:17:30 +0200

    Create StatsActivity

---
+++ android/app/src/main/java/com/ssg/readtrack/ui/StatsActivity.java
@@ -0,0 +1,51 @@
+package com.ssg.readtrack.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ssg.readtrack.R;
+
+public class StatsActivity extends AppCompatActivity {
+
+    TextView txtBooks, txtPages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        txtBooks = findViewById(R.id.txtBooks);
+        txtPages = findViewById(R.id.txtPages);
+
+        SharedPreferences prefs = getSharedPreferences("app", MODE_PRIVATE);
+        String token = prefs.getString("token", "");
+
+        if (token.isEmpty()) {
+            finish();
+            return;
+        }
+
+        ApiService api = RetrofitClient.getClient().create(ApiService.class);
+
+        api.getStats(token).enqueue(new Callback<StatsResponse>() {
+            @Override
+            public void onResponse(Call<StatsResponse> call, Response<StatsResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    StatsResponse stats = response.body();
+
+                    txtBooks.setText("Books read: " + stats.books_read);
+                    txtPages.setText("Pages read: " + stats.pages_read);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<StatsResponse> call, Throwable t) {
+            }
+        });
+    }
+}
