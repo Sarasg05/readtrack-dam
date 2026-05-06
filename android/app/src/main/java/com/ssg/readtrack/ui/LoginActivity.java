b1e428207f51d2a8c3b0bb5a0c0d264b07a5c316
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-06 12:21:52 +0200

    Implement login in LoginActivity (UI references)

--- android/app/src/main/java/com/ssg/readtrack/ui/LoginActivity.java
+++ android/app/src/main/java/com/ssg/readtrack/ui/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.ssg.readtrack.ui;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        EditText etUsername = findViewById(R.id.etUsername);
+        EditText etPassword = findViewById(R.id.etPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
