4c7d44d18949914c1807d5820d83ac8807a3f944
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-07 14:42:00 +0200

    Modify StatsFragment

--- android/app/src/main/java/com/ssg/readtrack/fragment/StatsFragment.java
+++ android/app/src/main/java/com/ssg/readtrack/fragment/StatsFragment.java
@@ -1,5 +1,6 @@
 package com.ssg.readtrack.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -8,14 +9,80 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.ssg.readtrack.R;
+import com.ssg.readtrack.model.StatsResponse;
+import com.ssg.readtrack.network.ApiService;
+import com.ssg.readtrack.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class StatsFragment extends Fragment {
 
-    @Nullable
+    TextView txtBooksCompleted;
+    TextView txtPagesRead;
+    TextView txtGoal;
+    TextView txtProgress;
+
+    ProgressBar progressBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        txtBooksCompleted = view.findViewById(R.id.txtBooksCompleted);
+        txtPagesRead = view.findViewById(R.id.txtPagesRead);
+        txtGoal = view.findViewById(R.id.txtGoal);
+        txtProgress = view.findViewById(R.id.txtProgress);
+
+        progressBar = view.findViewById(R.id.progressBar);
+
+        SharedPreferences prefs = requireActivity()
+                .getSharedPreferences("app", getContext().MODE_PRIVATE);
+
+        String token = prefs.getString("token", "");
+
+        ApiService api = RetrofitClient.getClient().create(ApiService.class);
+
+        api.getStats(token).enqueue(new Callback<StatsResponse>() {
+
+            @Override
+            public void onResponse(Call<StatsResponse> call, Response<StatsResponse> response) {
+
+                if (response.isSuccessful() && response.body() != null) {
+
+                    StatsResponse stats = response.body();
+
+                    txtBooksCompleted.setText(
+                            "Books completed: " + stats.books_completed
+                    );
+
+                    txtPagesRead.setText(
+                            "Pages read: " + stats.pages_read
+                    );
+
+                    txtGoal.setText(
+                            "Goal: " + stats.target_books
+                    );
+
+                    txtProgress.setText(
+                            "Progress: " + stats.progress + "%"
+                    );
+
+                    progressBar.setProgress(stats.progress);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<StatsResponse> call, Throwable t) {
+
+            }
+        });
+
+        return view;
     }
 }
--- android/app/src/main/java/com/ssg/readtrack/model/StatsResponse.java
+++ android/app/src/main/java/com/ssg/readtrack/model/StatsResponse.java
@@ -1,7 +1,7 @@
 package com.ssg.readtrack.model;
 
 public class StatsResponse {
-    public int book_completed;
+    public int books_completed;
     public int pages_read;
     public int target_books;
     public int progress;
