591406527fab0beb930b77b76e4132ec73435f6e
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-07 12:54:51 +0200

    Modify StatsResponse

--- android/app/src/main/java/com/ssg/readtrack/model/StatsResponse.java
+++ android/app/src/main/java/com/ssg/readtrack/model/StatsResponse.java
@@ -1,7 +1,8 @@
 package com.ssg.readtrack.model;
 
 public class StatsResponse {
-    public int year;
-    public int books_read;
+    public int book_completed;
     public int pages_read;
+    public int target_books;
+    public int progress;
 }
