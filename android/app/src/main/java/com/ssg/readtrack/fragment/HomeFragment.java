620ecd501d1d488d62090af5273b6aaa4030e57f
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-08 15:27:07 +0200

    Change HomeFragment

--- android/app/src/main/java/com/ssg/readtrack/fragment/HomeFragment.java
+++ android/app/src/main/java/com/ssg/readtrack/fragment/HomeFragment.java
@@ -32,117 +32,112 @@ import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView recyclerView;
-
-    TextView txtCurrentBook, txtCurrentPages, txtGoal;
+    TextView tvCurrentBook, tvBooks, tvPages, tvGoal;
     ProgressBar progressGoal;
+    RecyclerView rvRecommended;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        recyclerView = view.findViewById(R.id.recyclerBooks);
-        txtCurrentBook = view.findViewById(R.id.txtCurrentBook);
-        txtCurrentPages = view.findViewById(R.id.txtCurrentPages);
-        txtGoal = view.findViewById(R.id.txtGoal);
+        tvCurrentBook = view.findViewById(R.id.tvCurrentBook);
+        tvBooks = view.findViewById(R.id.tvBooks);
+        tvPages = view.findViewById(R.id.tvPages);
+        tvGoal = view.findViewById(R.id.tvGoal);
         progressGoal = view.findViewById(R.id.progressGoal);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        txtCurrentBook.setText("The Hobbit");
-        txtCurrentPages.setText("120 / 310 pages");
+        rvRecommended = view.findViewById(R.id.rvRecommended);
 
-        txtGoal.setText("7 / 20 books");
-        progressGoal.setProgress(35);
+        rvRecommended.setLayoutManager(
+                new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false)
+        );
 
-        loadBooks();
         loadHome();
+        loadRecommended();
 
         return view;
     }
 
-    private void loadBooks() {
+    private String getToken() {
+        SharedPreferences prefs = requireActivity()
+                .getSharedPreferences("app", getContext().MODE_PRIVATE);
 
-        ApiService apiService = RetrofitClient.getClient().create(ApiService.class);
-
-        Call<List<Book>> call = apiService.getBooks();
-
-        call.enqueue(new Callback<List<Book>>() {
-
-            @Override
-            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
-
-                if (response.isSuccessful() && response.body() != null) {
-
-                    List<Book> books = response.body();
-
-                    BookAdapter adapter = new BookAdapter(books, book -> {
-
-                        Intent intent = new Intent(getContext(), DetailActivity.class);
-
-                        intent.putExtra("title", book.title);
-                        intent.putExtra("author", book.author.name);
-                        intent.putExtra("total_pages", String.valueOf(book.total_pages));
-                        intent.putExtra("cover", book.cover);
-                        intent.putExtra("book_id", book.id);
-
-                        if (book.genres != null && !book.genres.isEmpty()) {
-                            intent.putExtra("genres", book.genres.get(0));
-                        }
-
-                        startActivity(intent);
-                    });
-
-                    recyclerView.setAdapter(adapter);
-
-                } else {
-                    Log.e("API", "Invalid response");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Book>> call, Throwable t) {
-                Log.e("API", t.getMessage());
-            }
-        });
+        return "Bearer " + prefs.getString("token", "");
     }
 
     private void loadHome() {
 
-        SharedPreferences prefs = requireActivity()
-                .getSharedPreferences("app", getContext().MODE_PRIVATE);
-
-        String token = prefs.getString("token", "");
-
         ApiService api = RetrofitClient.getClient().create(ApiService.class);
 
-        api.getHome(token).enqueue(new Callback<HomeResponse>() {
+        api.getHome(getToken()).enqueue(new Callback<HomeResponse>() {
 
             @Override
             public void onResponse(Call<HomeResponse> call, Response<HomeResponse> response) {
 
-                if (response.isSuccessful() && response.body() != null) {
+                if (!response.isSuccessful() || response.body() == null) return;
 
-                    HomeResponse data = response.body();
+                HomeResponse data = response.body();
 
-                    if (data.current_book != null) {
-                        txtCurrentBook.setText(data.current_book.title);
-                        txtCurrentPages.setText("Progress available");
-                    } else {
-                        txtCurrentBook.setText("No current book");
-                    }
-
-                    txtGoal.setText(data.books_read + " / " + data.goal);
-
-                    int progress = (data.goal == 0) ? 0 :
-                            (data.books_read * 100 / data.goal);
-
-                    progressGoal.setProgress(progress);
+                // CURRENT BOOK
+                if (data.current_book != null) {
+                    tvCurrentBook.setText(data.current_book.title);
+                } else {
+                    tvCurrentBook.setText("No current book");
                 }
+
+                // STATS
+                tvBooks.setText("Books: " + data.books_read);
+                tvGoal.setText("Goal: " + data.goal);
+
+                int progress = (data.goal == 0) ? 0 :
+                        (data.books_read * 100 / data.goal);
+
+                progressGoal.setProgress(progress);
             }
 
             @Override
             public void onFailure(Call<HomeResponse> call, Throwable t) {
+                Log.e("HOME", t.getMessage());
+            }
+        });
+    }
 
+    private void loadRecommended() {
+
+        ApiService api = RetrofitClient.getClient().create(ApiService.class);
+
+        api.getBooks().enqueue(new Callback<List<Book>>() {
+
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+
+                if (!response.isSuccessful() || response.body() == null) return;
+
+                List<Book> books = response.body();
+
+                BookAdapter adapter = new BookAdapter(books, book -> {
+
+                    Intent intent = new Intent(getContext(), DetailActivity.class);
+
+                    intent.putExtra("title", book.title);
+                    intent.putExtra("author", book.author.name);
+                    intent.putExtra("total_pages", String.valueOf(book.total_pages));
+                    intent.putExtra("cover", book.cover);
+                    intent.putExtra("book_id", book.id);
+
+                    if (book.genres != null && !book.genres.isEmpty()) {
+                        intent.putExtra("genres", book.genres.get(0));
+                    }
+
+                    startActivity(intent);
+                });
+
+                rvRecommended.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                Log.e("BOOKS", t.getMessage());
             }
         });
     }
