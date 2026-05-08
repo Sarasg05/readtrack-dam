bff02096ae742df112b0435ee3763344682f5e3c
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-08 12:00:41 +0200

    Create SplashActivity

---
+++ android/app/src/main/java/com/ssg/readtrack/ui/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.ssg.readtrack.ui;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs =
+                getSharedPreferences("app", MODE_PRIVATE);
+
+        String token = prefs.getString("token", "");
+
+        if (token.isEmpty()) {
+
+            startActivity(
+                    new Intent(this, LoginActivity.class)
+            );
+
+        } else {
+
+            startActivity(
+                    new Intent(this, MainActivity.class)
+            );
+        }
+
+        finish();
+    }
+}
