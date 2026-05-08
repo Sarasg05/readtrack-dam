17de24e7eab866f2bf53d0b2129aa1180aee535f
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-08 11:54:24 +0200

    Fix register

--- android/app/src/main/java/com/ssg/readtrack/ui/RegisterActivity.java
+++ android/app/src/main/java/com/ssg/readtrack/ui/RegisterActivity.java
@@ -1,10 +1,10 @@
 package com.ssg.readtrack.ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,8 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.ssg.readtrack.R;
-import com.ssg.readtrack.model.LoginRequest;
 import com.ssg.readtrack.model.LoginResponse;
+import com.ssg.readtrack.model.RegisterRequest;
 import com.ssg.readtrack.network.ApiService;
 import com.ssg.readtrack.network.RetrofitClient;
 
@@ -29,13 +29,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         EditText etUsername = findViewById(R.id.etUsername);
         EditText etPassword = findViewById(R.id.etPassword);
-        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
+        TextView txtLogin = findViewById(R.id.txtLogin);
 
-        btnLogin.setOnClickListener(v -> {
+        btnRegister.setOnClickListener(v -> {
 
             String username = etUsername.getText().toString();
             String password = etPassword.getText().toString();
@@ -47,35 +48,40 @@ public class RegisterActivity extends AppCompatActivity {
 
             ApiService api = RetrofitClient.getClient().create(ApiService.class);
 
-            LoginRequest request = new LoginRequest(username, password);
+            RegisterRequest request = new RegisterRequest(username, password);
 
-            api.login(request).enqueue(new Callback<LoginResponse>() {
+            api.register(request).enqueue(new Callback<Void>() {
                 @Override
-                public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                public void onResponse(Call<Void> call, Response<Void> response) {
 
-                    if (response.isSuccessful() && response.body() != null) {
-
-                        String token = response.body().token;
-
-                        SharedPreferences prefs = getSharedPreferences("app", MODE_PRIVATE);
-                        prefs.edit().putString("token", token).apply();
+                    if (response.isSuccessful()) {
 
                         Toast.makeText(RegisterActivity.this, "User created", Toast.LENGTH_SHORT).show();
 
-                        // Ir a MainActivity
-                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        // Ir a LoginActivity
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                         finish();
 
                     } else {
-                        Toast.makeText(RegisterActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(RegisterActivity.this, "Error creating user", Toast.LENGTH_SHORT).show();
                     }
                 }
 
                 @Override
-                public void onFailure(Call<LoginResponse> call, Throwable t) {
+                public void onFailure(Call<Void> call, Throwable t) {
                     Toast.makeText(RegisterActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
+        });
+
+        txtLogin.setOnClickListener(v -> {
+
+            startActivity(new Intent(
+                    RegisterActivity.this,
+                    LoginActivity.class
+            ));
+
+            finish();
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
