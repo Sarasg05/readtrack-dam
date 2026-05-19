47853c283ebb980d36e0d630999ac86adb789153
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-19 12:07:51 +0200

    Fix SplashActivity

--- android/app/src/main/java/com/ssg/readtrack/ui/SplashActivity.java
+++ android/app/src/main/java/com/ssg/readtrack/ui/SplashActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ssg.readtrack.model.User;
+import com.ssg.readtrack.network.ApiService;
+import com.ssg.readtrack.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -17,19 +25,55 @@ public class SplashActivity extends AppCompatActivity {
 
         String token = prefs.getString("token", "");
 
+        // NO TOKEN
         if (token.isEmpty()) {
 
             startActivity(
                     new Intent(this, LoginActivity.class)
             );
 
-        } else {
-
-            startActivity(
-                    new Intent(this, MainActivity.class)
-            );
+            finish();
+            return;
         }
 
-        finish();
+        // VALIDATE TOKEN
+        ApiService api = RetrofitClient.getClient().create(ApiService.class);
+
+        api.getMe("Bearer " + token).enqueue(new Callback<User>() {
+
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+                if (response.isSuccessful()) {
+
+                    // TOKEN OK
+                    startActivity(
+                            new Intent(SplashActivity.this, MainActivity.class)
+                    );
+
+                } else {
+
+                    // TOKEN INVALID
+                    prefs.edit().remove("token").apply();
+
+                    startActivity(
+                            new Intent(SplashActivity.this, LoginActivity.class)
+                    );
+                }
+
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+                // ERROR -> LOGIN
+                startActivity(
+                        new Intent(SplashActivity.this, LoginActivity.class)
+                );
+
+                finish();
+            }
+        });
     }
 }
